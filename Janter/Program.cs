805fe26e994f066ed11005b6daa5c7778cbf4fe1
Opilane TTHK <opilane@tthk.ee>
2025-09-04 11:28:10 +0300

    4-SEP-25 muutuja, andmetüübid, if

---
+++ Janter/Program.cs
@@ -0,0 +1,92 @@
+﻿/*
+int käimasolevAasta = 2025;
+int olenNiiVana = 16;
+int tehteTulemus = käimasolevAasta + olenNiiVana;
+
+//float pikkus = 2.5f;
+
+string sõnum = "Teie vanus on: ";
+
+//bool miskion = false;
+
+//Console.WriteLine(tehteTulemus + sõnum);
+//Console.WriteLine("sõnum");
+//Console.WriteLine("sõnum");
+//Console.WriteLine("sõnum");
+//Console.WriteLine("sõnum");
+//Console.WriteLine("oioioioioioi");
+//Console.WriteLine("sõnum");
+//Console.WriteLine("sõnum");
+//Console.WriteLine("sõnum");
+//Console.WriteLine("sõnum");
+//Console.WriteLine("sõnum");*/
+
+
+//int arv1 = 0;
+//int arv2 = 0;
+//Console.WriteLine("Tere, palun sisesta esimene arv: ");
+//arv1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Tere, palun sisesta teine arv: ");
+//arv2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Missugust tehet soovid teha? Kirjuta kas: + - / *");
+//string tehtetüüp = Console.ReadLine();
+//int tehe = 0;
+//if (tehtetüüp == "+"  /*tingimus siia*/)
+//{ 
+//    tehe = arv1 + arv2;
+//}
+
+//Console.WriteLine($"{tehtetüüp}Tehte tulemus: {tehe} ");
+
+
+//// + liidame
+//// - lahutame
+//// / jagame
+//// * korrutame
+
+//int arv1 = 0;
+//int arv2 = 0;
+//Console.WriteLine("Tere, palun sisesta esimene arv: ");
+//arv1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Tere, palun sisesta teine arv: ");
+//arv2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Missugust tehet soovid teha? Kirjuta kas: + - * /");
+//string tehtetüüp = Console.ReadLine();
+//int tehe = 0;
+//if (tehtetüüp == "+" /*timgimus siia*/)
+//{
+//    tehe = arv1 + arv2;
+//}
+//Console.WriteLine($"Tehte tulemus: {tehe}");
+
+
+//int arv1 = 0;
+//int arv2 = 0;
+//Console.WriteLine("Tere, palun sisesta esimene arv: ");
+//arv1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Tere, palun sisesta teine arv:");
+//arv2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Missugust tehet soovid teha? Kirjuta kas : + - * /");
+//string tehtetüüp = Console.ReadLine();
+//int tehe = 0;
+//if (tehtetüüp == "+" /* tingimus siia*/)
+//{
+//    tehe = arv1 + arv2;
+//}
+//Console.WriteLine($"Tehte tulemus: {tehe}");
+
+
+//int arv1 = 0;
+//int arv2 = 0;
+//Console.WriteLine("Tere, palun sisesta esimene arv:");
+//arv1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Tere, palun sisesta teine arv:");
+//arv2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Missugust tehet soovid teha? kirjuta kas + - * / ");
+//string tehtetüüp = Console.ReadLine.();
+//int tehe = arv1 * arv2;
+//if (tehtetüüp == "+" /* tingimus siia*/)
+//{
+//    tehe = arv1 + arv2;
+//}
+//Console.WriteLine($"Tehte tulemus: {tehe}");
